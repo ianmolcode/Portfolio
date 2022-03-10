fb6a77f9708e48db71ca85952437273d680bfe70
Anmol Narang <nanmol0406@gmail.com>
2022-03-11 00:20:30 +0530

    Project D1-2

--- app/src/main/java/com/example/mycv/MainActivity.java
+++ app/src/main/java/com/example/mycv/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import home.FragmentHome;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getActionBar().hide();
+        setHomeFragment();
+    }
+
+    void setHomeFragment(){
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,new FragmentHome()).commit();
+
     }
 }
